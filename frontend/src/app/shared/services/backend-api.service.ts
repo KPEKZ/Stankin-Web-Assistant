f3357b97e9d59be8e5f8be46d6f4f1c6dcde8da4
eduard.kopylov <eduard.kopylov@qoollo.com>
2022-05-27 23:13:37 +0300

    feature/T-1_backend_api_service, add base models and endpoints

--- frontend/src/app/shared/services/backend-api.service.ts
+++ frontend/src/app/shared/services/backend-api.service.ts
@@ -1,7 +1,24 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Employee } from '../models/employee';
+import { EmployeeDto } from '../models/employee-dto';
 
 @Injectable()
 export class BackendApiService {
+  private _stankinUrl = 'https://rinh-api.kovalev.team/';
 
-  constructor() { }
+  constructor(private readonly _http: HttpClient) {}
+
+  public getEmployee(): Observable<Employee[]> {
+    return this._http.get<Employee[]>(this._stankinUrl + 'employee');
+  }
+
+  public getEmployeeByid(id:number): Observable<Employee> {
+    return this._http.get<Employee>(this._stankinUrl + 'employee/' + id);
+  }
+
+  public getEmployeeDtoById(id: number): Observable<EmployeeDto> {
+    return this._http.get<EmployeeDto>(this._stankinUrl + 'employee/dto/' + id);
+  }
 }
